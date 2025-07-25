dc10193dc64745e2032d4f028d36cc654f835fd5
Aadit Antony <aaditantony1112@gmail.com>
2025-07-25 12:47:40 +0530

    the production items has been completed and worked in the postman of the mac

---
+++ src/main/java/com/example/demo/productCreation/ProductRequest.java
@@ -0,0 +1,35 @@
+package com.example.demo.productCreation;
+
+public class ProductRequest {
+
+    private String name;
+    private double price;
+    private String category;
+
+    //Getter and Setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
